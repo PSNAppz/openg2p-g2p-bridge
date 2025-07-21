dbd137fad32082091724c03186353fca6ba4bea2
PSNAppZ <thepsnarayanan@gmail.com>
2025-07-21 23:24:30 +0530

    Temporarily override JWT signature validation in upload_mt940 method for testing purposes

--- openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/account_statement.py
+++ openg2p-g2p-bridge-api/src/openg2p_g2p_bridge_api/controllers/account_statement.py
@@ -48,7 +48,8 @@ class AccountStatementController(BaseController):
 
     async def upload_mt940(
         self,
-        is_signature_valid: Annotated[bool, Depends(JWTSignatureValidator())],
+        # is_signature_valid: Annotated[bool, Depends(JWTSignatureValidator())],
+        is_signature_valid: bool = True, # TODO: Overriding signature validation for testing purposes
         statement_file: UploadFile = File(...),
     ) -> AccountStatementResponse:
         _logger.info("Uploading statement file")
